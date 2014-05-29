558ac06b10c03a961c948199277180c326906a9a
ZhangYanMing <ymzhang@ingenic.cn>
2014-05-29 09:56:19 +0800

    add README

--- logwatch.h
+++ logwatch.h
@@ -1,8 +1,17 @@
 /*
- * config.h
+ *  Copyright (C) 2013, Zhang YanMing <jamincheung@126.com>
  *
- *  Created on: Jun 29, 2013
- *      Author: jamin
+ *  ingenic log record tool
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under  the terms of the GNU General  Public License as published by the
+ *  Free Software Foundation;  either version 2 of the License, or (at your
+ *  option) any later version.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  */
 
 #ifndef LOGWATCH_H_
